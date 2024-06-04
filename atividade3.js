4eda82a667705e50645aa9b04927c79e18edcb56
joao4321 <89591695+joao4321@users.noreply.github.com>
2024-06-04 17:13:51 -0300

    Update atividade3.js

--- atividade3.js
+++ atividade3.js
@@ -1,60 +1,36 @@
-import React, { useState } from 'react';
-import './Contador.css';
-import imagemHomem from './Imagens/homem.webp';
-import ImagemMulher from './Imagens/mulher.jpg';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-function ContadorPessoas() {
-	const [total, setTotal] = useState(0);
-	const [homens, setHomens] = useState(0);
-	const [mulheres, setMulheres] = useState(0);
+function Letreiro() {
+    const [texto, setTexto] = React.useState("");
+    const [contador, setContador] = React.useState(0);
+    const mensagem = "Venha estudar na FATEC!";
 
-	const AddPerson = (gender) => {
-		if (gender === 'homens') {
-			setHomens(homens + 1);
-			setTotal(total + 1);
-		} else if (gender === 'mulheres') {
-			setMulheres(mulheres + 1);
-			setTotal(total + 1);
-		}
-	};
+    React.useEffect(() => {
+      const intervalo = setInterval(() => {
+        if (contador < mensagem.length) {
+          setTexto((prevTexto) => prevTexto + mensagem[contador]);
+          setContador((prevContador) => prevContador + 1);
+        } else {
+          clearInterval(intervalo);
+          setTimeout(() => {
+            setTexto("");
+            setContador(0);
+          }, 2000);
+        }
+      }, 200);
+      return () => clearInterval(intervalo);
+    }, [contador, mensagem]);
 
-	const RemovePerson = (gender) => {
-		if (gender === 'homens' && homens > 0) {
-			setHomens(homens - 1);
-			setTotal(total - 1);
-		} else if (gender === 'mulheres' && mulheres > 0) {
-			setMulheres(mulheres - 1);
-			setTotal(total - 1);
-		}
-	};
-
-	const Refresh = () => {
-		setTotal(0);
-		setHomens(0);
-		setMulheres(0);
-	};
-
-	const isNegative = total < 0;
-
-	return (
-		<div className='container'>
-			<h1>Total: {total}</h1>
-			{isNegative && <p style={{ color: 'red' }}>Número total de pessoas não pode ser negativo</p>}
-			<div class='buttons-container'>
-				<img class="img" src={imagemHomem} alt={'Homem'} />
-				<h2>Homens: {homens}</h2>
-				<button class="botao" onClick={() => AddPerson('homens')}>+</button>
-				<button class="botao" onClick={() => RemovePerson('homens')}>-</button>
-			</div>
-			<div class='buttons-container'>
-				<img class="img" src={ImagemMulher} alt={'Mulher'} />
-				<h2>Mulheres: {mulheres}</h2>
-				<button class="botao" onClick={() => AddPerson('mulheres')}>+</button>
-				<button class="botao" onClick={() => RemovePerson('mulheres')}>-</button>
-			</div>
-			<button className='refresh-button' onClick={Refresh}>Limpar</button>
-		</div>
-	);
+    return <h1>{texto}</h1>;
 }
 
-export default ContadorPessoas;
+function App() {
+    return (
+        <div>
+            <Letreiro />
+        </div>
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
